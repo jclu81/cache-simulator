d17237bea1e348f5b95b2922b2615478d34d70fc
will <jclu81@gmail.com>
2017-11-20 19:20:06 -0800

    Initial Commit

---
+++ cacheSimulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cacheSimulator
+//
+//  Created by Jingcheng Lu on 11/20/17.
+//  Copyright © 2017 Jingcheng Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
